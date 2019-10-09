ac1e980d00b4e9e607674d0be23a31a0f329d300
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-10-09 15:46:17 -0600

    test: configure

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,16 @@
 module.exports = {
+  collectCoverage: true,
   projects: [
     {
+      displayName: "test",
+      preset: "jest-preset-gatsby/typescript",
+      snapshotSerializers: ["jest-serializer-react-helmet"],
+      collectCoverage: true
+    },
+    {
       displayName: "lint:prettier",
-      preset: "jest-runner-prettier"
+      preset: "jest-runner-prettier",
+      testPathIgnorePatterns: ["/node_modules/", "/coverage/"]
     }
   ]
 };
