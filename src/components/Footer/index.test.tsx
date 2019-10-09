61d22b79f42aa4e6d3dc025958cfb3ed43d53678
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-10-09 15:46:39 -0600

    test: sanity test Footer

---
+++ src/components/Footer/index.test.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import renderer from "react-test-renderer";
+import { useStaticQuery } from "gatsby";
+import Footer from ".";
+
+describe("Footer", () => {
+  beforeEach(() => {
+    (useStaticQuery as jest.Mock).mockImplementationOnce(() => ({
+      site: {
+        siteMetadata: {
+          title: "Example Title"
+        }
+      }
+    }));
+  });
+
+  it("renders as expected", () => {
+    const tree = renderer.create(<Footer />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
