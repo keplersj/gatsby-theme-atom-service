d90fcb3a326f283f1a22078af74a1e3e94349901
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-10-10 01:21:32 -0600

    test: sanity check Header component

---
+++ src/components/Header/index.test.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import renderer from "react-test-renderer";
+import { useStaticQuery } from "gatsby";
+import Header from ".";
+
+describe("Header", () => {
+  beforeEach(() => {
+    (useStaticQuery as jest.Mock).mockImplementationOnce(() => ({
+      site: {
+        siteMetadata: {
+          title: "Example Site",
+          description: "This is an example site description."
+        }
+      },
+      logo: {
+        publicURL: "/static/logo.svg"
+      }
+    }));
+  });
+
+  it("renders as expected", () => {
+    const tree = renderer.create(<Header />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
