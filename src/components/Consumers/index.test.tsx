526075c5dcb84684a7af7dc757d037b59f5cc192
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-10-10 01:14:07 -0600

    test: sanity check Consumers component

---
+++ src/components/Consumers/index.test.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import renderer from "react-test-renderer";
+import { useStaticQuery } from "gatsby";
+import Consumers from ".";
+
+describe("Consumers", () => {
+  beforeEach(() => {
+    (useStaticQuery as jest.Mock).mockImplementationOnce(() => ({
+      consumers: {
+        edges: [
+          {
+            node: {
+              id: "123",
+              author: "Namey McNameson",
+              title: "Example Consumer",
+              url: "https://example.dev/consumer",
+              code: "https://github.dev/example/consumer"
+            }
+          }
+        ]
+      }
+    }));
+  });
+
+  it("renders as expected", () => {
+    const tree = renderer.create(<Consumers />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
