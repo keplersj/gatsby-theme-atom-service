6752f4190a4fd83aa2a129089d043e0d45c1c1eb
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-12-31 15:15:54 -0700

    test(example-site): working with test data

--- gatsby-theme-atom-service/gatsby-config.js
+++ gatsby-theme-atom-service/gatsby-config.js
@@ -6,6 +6,26 @@ module.exports = options => {
   const basePath = options.basePath || "/";
 
   return {
+    siteMetadata: {
+      title: "Example Atom Service",
+      siteUrl: "https://example.dev",
+      description: "Gatsby site built using gatsby-theme-atom-service",
+      color: {
+        primary: "#efeae1",
+        secondary: "#66595c",
+        active: "#81c08b"
+      },
+      nav: [
+        {
+          name: "Atom Package Manager",
+          url: "https://atom.dev/packages/example"
+        },
+        {
+          name: "GitHub",
+          url: "https://github.dev/example/example"
+        }
+      ]
+    },
     plugins: [
       "gatsby-plugin-material-ui",
       "gatsby-plugin-lodash",
